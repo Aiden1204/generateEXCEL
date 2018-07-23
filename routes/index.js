9f47e70290fc4f5f1c0610d04a628ce899172633
Aiden <15000487124@163.com>
2018-07-23 17:39:06 +0800

    初次提交

---
+++ routes/index.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+
+var xlsx = require('node-xlsx');
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('test');
+});
+
+router.post('/login', function(req, res, next) {
+    console.log(req.body);
+    // 读xlsx
+    var obj = xlsx.parse("E:/resut.xls");
+    console.log(obj[0].data);
+    var x = [req.body.name, req.body.age];
+    obj[0].data.push(x);
+    var data = obj;
+    // var data = [
+    //     {
+    //         name : 'sheet1',
+    //         data : [
+    //             [
+    //                 '1',
+    //                 'Michael',
+    //             ],
+    //             [
+    //                 '2',
+    //                 'Jordan'
+    //             ]
+    //         ]
+    //     }
+    // ];
+    // for (var i = 0; i < obj[0].data.length; i++){
+    //     data[0].data.push(obj[0].data[i]);
+    // }
+
+
+    // 写xlsx
+    var buffer = xlsx.build(data);
+    fs.writeFile('E:/resut.xls', buffer, function (err)
+        {
+            if (err)
+                throw err;
+            console.log('Write to xls has finished');
+
+
+        }
+    );
+
+    res.send("1111");
+});
+module.exports = router;
